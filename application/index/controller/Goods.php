80d3901090c731267eec123933b44c6a9545ee20
zlr <1029422171@qq.com>
2019-10-15 17:06:27 +0800

    增加商品模块

---
+++ application/index/controller/Goods.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace app\index\controller;
+
+
+class Goods
+{
+    public function addLx(){
+        $shop_id = $_REQUEST['shopid'];
+        $lx_name = $_REQUEST['lxname'];
+        $full = $_REQUEST['full'];
+        
+    }
+
+    public function updateLx(){
+
+    }
+
+    public function delLx(){
+
+    }
+
+    public function getLxList(){
+
+    }
+
+    public function addGoods(){
+
+    }
+
+    public function updateGoods(){
+
+    }
+
+    public function delGoods(){
+
+    }
+
+    public function getGoodsList(){
+
+    }
+    
+    public function getGoodsInfo(){
+
+    }
+}
