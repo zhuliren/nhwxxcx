1dea13227e8b2bec52b98fdd733d5ac94c2dd346
zlr <1029422171@qq.com>
2019-10-30 17:09:50 +0800

    更新

--- application/index/controller/Goods.php
+++ application/index/controller/Goods.php
@@ -12,7 +12,6 @@ class Goods
         $shop_id = $_REQUEST['shopid'];
         $lx_name = $_REQUEST['lxname'];
         $full = $_REQUEST['full'];
-        
     }
 
     public function updateLx(){
@@ -52,10 +51,11 @@ class Goods
         $lxdata = Db::table('tbl_lx')->where('used',1)->where('shopid',$shopid)->column('id,name,icon as iconName');
         $lx = array();
         $goodswithlx = array();
+        $i=0;
         foreach ($lxdata as $item){
             $lxid = $item['id'];
             $item['isDiscount']=false;
-            $item['id']='link'.$item['id'];
+            $item['id']='link'.$i;
             $lx[] = $item;
             $goodsdata = Db::table('tbl_goods')->where('used',1)->where('lxid',$lxid)->column('id as no,img as iconName,name as titleName,specs as ingredient,introduce as "describe",uprice as nowPrice');
             $goods = array();
@@ -67,6 +67,7 @@ class Goods
             }
             $newitem = array('id'=>$item['id'],'linkName'=>$item['name'],'detailed'=>$goods);
             $goodswithlx[] = $newitem;
+            $i++;
         }
         $returndata = array('linkList'=>$lx,'detailedList'=>$goodswithlx);
         $data = array('status' => 0, 'msg' => '成功', 'data' => $returndata);
